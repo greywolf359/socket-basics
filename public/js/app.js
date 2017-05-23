52e40448e645ffa706ba971a8257a72a2b9ff4a5
Glenn Kirchner <greywolf359@gmail.com>
2017-05-23 12:02:13 -0700

    add input for messages on frontend

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,20 @@ var socket = io();
 
 socket.on('connect', ()=>{
 	console.log("connected to socket.io server");
+});
+
+socket.on("message",function(message){
+	console.log('new message:');
+	console.log(message);
 })
+
+//handles submitting of new message
+
+var $form = $('#message-form');
+$form.on('submit', (event)=>{
+	event.preventDefault();
+	socket.emit('message', {
+		text: $form.find('input[name=message]').val()
+	})
+	$form.find('input[name=message]').val('');
+});
--- server.js
+++ server.js
@@ -5,8 +5,21 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
+
 io.on('connection', function(socket){
 	console.log("user connected via socket.io");
+	socket.on('message', function(message){
+		console.log('message received: ' + message.text);
+		socket.broadcast.emit("message", message.text);
+		console.log("executed after broadcast...."); 
+	})
+
+		//broadcast to everyone but sender
+		//io.emit - sends to every single person including sender
+
+	socket.emit('message', {
+		text: "welcome to chat application"
+	})
 })
 
 http.listen(PORT,()=>{
